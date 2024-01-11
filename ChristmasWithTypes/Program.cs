28f8ba710cddb130fc715b3ca4d731d9bbc29cd7
Maberry00 <maberry30@gmail.com>
2024-01-11 09:35:27 -0600

    fixed tree height

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -17,7 +17,7 @@ namespace ChristmasWithTypes
             xmas.Presents =  new string[3]{"Candy","Poptarts","Pizza"}; 
 
             //TODO Set the TreeHeight to 10
-            xmas.TreeHeight = 20;
+            xmas.TreeHeight = 10;
 
             Console.WriteLine($"This year christmas falls on {xmasDay} \n");
             Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
